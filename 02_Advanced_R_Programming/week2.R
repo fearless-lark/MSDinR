bb9620f58a534ff8f6cce727c89b851d9f350a24
Oleh <olehyashchuk@gmail.com>
2019-01-16 18:36:29 +0200

    Course 2: Week 3 theory + ReadMe

--- 02_Advanced_R_Programming/week2.R
+++ 02_Advanced_R_Programming/week2.R
@@ -387,3 +387,22 @@ beera({
 
 # The error handling process slows down your program by orders of magnitude!
 
+
+# swirl part --------------------------------------------------------------
+map_chr(c(5, 3, 4), int_to_string)
+map_lgl(c(1, 2, 3, 4, 5), gt, b = 3)
+map_if(c(1, 2, 3, 4), is_even, square)
+map_at(c(4,6,2,3,8), c(1,3,4), square)
+map2_chr(letters, 1:26, paste)
+reduce(c(1,3,5,7), add_talk)
+reduce(c("a", "b", "c", "d"), paste_talk)
+reduce_right(c("a", "b", "c", "d"), paste_talk)
+has_element(random_ints, 45)
+detect(random_ints, is_even)
+detect_index(random_ints, is_even)
+keep(random_ints, is_even)
+discard(random_ints, is_even)
+every(random_ints, is_even)
+gt_10 <- partial(gt, b = 10)
+gt_10(11)
+walk(mark_antony, message)
---
+++ 02_Advanced_R_Programming/week3.R
@@ -0,0 +1,89 @@
+# Debugging and Profiling -------------------------------------------------
+
+# Debugging ---------------------------------------------------------------
+# traceback()
+check_n_value <- function(n) {
+    if(n > 0) {
+        stop("n should be <= 0")
+    }
+}
+error_if_n_is_greater_than_zero <- function(n){
+    check_n_value(n)
+    n
+}
+error_if_n_is_greater_than_zero(5)
+traceback()
+
+
+# browser()
+check_n_value <- function(n) {
+    if(n > 0) {
+        browser()  ## Error occurs around here
+        stop("n should be <= 0")
+    }
+}
+error_if_n_is_greater_than_zero(5)
+
+
+# trace()
+# 1
+trace("check_n_value")
+error_if_n_is_greater_than_zero(5)
+
+# 2
+as.list(body(check_n_value))
+as.list(body(check_n_value)[[2]])
+trace("check_n_value", browser, at = list(c(2, 3)))
+body(check_n_value)
+
+# 3
+trace("check_n_value", quote({
+    if(n == 5) {
+        message("invoking the browser")
+        browser()
+    }
+}), at = 2)
+body(check_n_value)
+
+# 4
+trace("glm", browser, at = 4, where = asNamespace("stats"))
+body(stats::glm)[1:5]
+
+
+# debug()
+# debug(f) ~= trace(f, browser)
+debug(lm)
+debugonce()
+
+# recover()
+options(error = recover)
+error_if_n_is_greater_than_zero(5)
+
+
+# Profiling ---------------------------------------------------------------
+# microbenchmark
+library(ggplot2)
+library(microbenchmark)
+tmp <- microbenchmark(a <- rnorm(1000), 
+                      b <- mean(rnorm(1000)))
+autoplot(tmp)
+
+
+# profvis
+library(profvis)
+datafr <- chicagoNMMAPS
+threshold <- 27
+
+profvis({
+    highest_temp <- c()
+    record_temp <- c()
+    for(i in 1:nrow(datafr)){
+        highest_temp <- max(highest_temp, datafr$temp[i])
+        record_temp[i] <- datafr$temp[i] >= threshold & 
+            datafr$temp[i] >= highest_temp
+    }
+    datafr <- cbind(datafr, record_temp)
+})
+# Non-standard evaluation -------------------------------------------------
+
+
